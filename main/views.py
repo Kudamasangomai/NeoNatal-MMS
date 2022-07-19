8d3a61bd88187b44556d7305a2db61c28906a1b4
Kuda Masangomai <kudam775@gmail.com>
2022-07-19 23:43:20 +0200

    dhasboard done

--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from child.models import child
+from django.db.models import Q ,Count
 from patients.models import *
+from django.db.models.functions import TruncMonth
 
 # Create your views here.
 
@@ -13,7 +15,13 @@ def dashboard(request):
         'totalpatients': patient.objects.all().count,
         'totalchildren':child.objects.all().count,
         'lowsurvival':child.objects.filter(heartrate__lte = 1).count(),
-        'lowsurvivalq':child.objects.filter(heartrate__lte = 1)
+        'lowsurvivalq':child.objects.filter(heartrate__lte = 1),
+
+       'a': child.objects
+    .annotate(month=TruncMonth('Dob'))  # Truncate to month and add to select list
+    .values('month')                          # Group By month
+    .annotate(c=Count('id'))                  # Select the count of the grouping
+    .values('month', 'c') 
 	# 'my_books' : books.objects.filter(category = categoryid),
 	# 'category_name':category.objects.get(id=categoryid),
 	# #'count':books.objects.filter(category=categoryid).count()
